b1927fe3eb6db77cb3a5b5cabcd1851ef18b3e2e
saeedabdilar <saeed.abdilar@gmail.com>
2020-05-15 01:46:04 +0430

    [feature] add variable.js file. Name of constant variables are defined here

---
+++ src/config/variables.js
@@ -0,0 +1,8 @@
+/*
+* Name of constant variables are defined here
+* */
+
+export const ACCESS_TOKEN = "ACCESS_TOKEN";
+export const REFRESH_TOKEN = "REFRESH_TOKEN";
+export const CLIENT_ID = "CLIENT_ID";
+export const USER_INFO = "USER_INFO";
