ac428a889e63edbbfd53e6bca7c9ae006ab083fb
saeedabdilar <saeed.abdilar@gmail.com>
2020-05-15 02:17:38 +0430

    [feature] add error handler function

--- src/helper/functions.js
+++ src/helper/functions.js
@@ -3,3 +3,23 @@
 * */
 
 import { toast } from "react-toastify";
+import errorMessage from "../assets/data/error";
+
+/*
+* Show Error toase message
+* @param {Object} error - The API error response
+* */
+export function errorHandler(error) {
+  if (!navigator.onLine) {
+    toast.error(errorMessage["connection"]);
+  } else if (error.response && error.response.status) {
+    if (error.response.status === 400 || error.response.status === 404) {
+      toast.error(<span dangerouslySetInnerHTML={{__html: error.response.data.error_description || errorMessage[error.response.status]}} />);
+    } else {
+      toast.error(errorMessage[error.response.status]);
+    }
+  } else {
+    toast.error(errorMessage["unknown"]);
+  }
+}
+
