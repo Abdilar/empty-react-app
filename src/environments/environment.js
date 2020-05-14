53721249456c9e5031e0fbf088d70f541b454676
saeedabdilar <saeed.abdilar@gmail.com>
2020-05-15 01:56:12 +0430

    [feature] add environment file. Base url is defined here

---
+++ src/environments/environment.js
@@ -0,0 +1,5 @@
+/*
+* Development base url is defined here
+* */
+
+export const BASE_URL = "http://localhost:3000/api";
---
+++ src/environments/environment.prod.js
@@ -0,0 +1,5 @@
+/*
+* Production base url is defined here
+* */
+
+export const BASE_URL = "http://localhost:3000";
