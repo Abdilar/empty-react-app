40ed635615e1ddca49362df64c7b45360a187033
saeedabdilar <saeed.abdilar@gmail.com>
2020-05-15 06:19:41 +0430

    [feature] Add toast container to app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ToastContainer, toast } from "react-toastify";
 import AppRouting from './app.route';
 
 import 'normalize.css/normalize.css';
+import "react-toastify/dist/ReactToastify.css";
 import './assets/styles/global.scss';
 
-ReactDOM.render(<AppRouting />, document.getElementById('app'));
+const App = () => (
+  <React.Fragment>
+    <AppRouting />
+    <ToastContainer
+        autoClose={5000}
+        position={toast.POSITION.TOP_CENTER}
+        newestOnTop
+        closeOnClick
+        pauseOnVisibilityChange
+        draggable={false}
+        pauseOnHover
+    />
+  </React.Fragment>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
