26ff620ffd152fcc8ece02109f2c5d48359bb381
saeedabdilar <saeed.abdilar@gmail.com>
2020-05-17 07:47:58 +0430

    [feature] add style loader to webpack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,6 +22,10 @@ module.exports = {
         loader: "babel-loader",
         include: PATH.join(__dirname, '..', 'src'),
         exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
