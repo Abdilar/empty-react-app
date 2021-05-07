cffbab6ed44bfc189a5c5435f8dbafc63b9fafc7
saeedabdilar <saeed.abdilar@gmail.com>
2021-05-07 20:37:02 +0430

    [fix]  variable declaration fixed

--- src/config/toast.config.js
+++ src/config/toast.config.js
@@ -1,2 +1,2 @@
-export const AUTO_CLOSE_TIME: Number(process.env.REACT_APP_TOAST_AUTO_CLOSE) || 5000,
-export const LIMIT: process.env.REACT_APP_TOAST_LIMIT || 3
+export const AUTO_CLOSE_TIME = Number(process.env.REACT_APP_TOAST_AUTO_CLOSE) || 5000;
+export const LIMIT = process.env.REACT_APP_TOAST_LIMIT || 3;
