3ab69106dd2c9a6e7b63aad9817c970d72bfbea4
saeedabdilar <saeed.abdilar@gmail.com>
2021-05-07 17:55:13 +0430

    [refactor] config constant to variables.config added

--- src/config/variables.config.js
+++ src/config/variables.config.js
@@ -2,11 +2,13 @@
 * Name of constant variables are defined here
 * */
 export const ACCESS_TOKEN = 'ACCESS_TOKEN';
+export const APP_ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT || 'development';
 export const APP_DEFAULT_LANGUAGE = process.env.REACT_APP_DEFAULT_LANGUAGE || 'en';
 export const APP_DIR = 'APP_DIR';
 export const APP_LANGUAGE = 'APP_LANGUAGE';
 export const APP_TOKEN = 'token';
 export const AXIOS_TIMEOUT = Number(process.env.REACT_APP_AXIOS_TIMEOUT) || 120000;
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const CONFIG = 'CONFIG';
 export const IS_LOGGED_IN = 'IS_LOGGED_IN';
 export const REFRESH_TOKEN = 'REFRESH_TOKEN';
