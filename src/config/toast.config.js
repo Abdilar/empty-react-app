f0001b55667fbbd083f923e3dfe856983f766174
saeedabdilar <saeed.abdilar@gmail.com>
2021-05-07 19:07:22 +0430

    [refactor] refactor toast.config

--- src/config/toast.config.js
+++ src/config/toast.config.js
@@ -1,4 +1,2 @@
-export const TOAST = {
-  AUTO_CLOSE_TIME: Number(process.env.REACT_APP_TOAST_AUTO_CLOSE) || 5000,
-  LIMIT: process.env.REACT_APP_TOAST_LIMIT || 3
-};
+export const AUTO_CLOSE_TIME: Number(process.env.REACT_APP_TOAST_AUTO_CLOSE) || 5000,
+export const LIMIT: process.env.REACT_APP_TOAST_LIMIT || 3
